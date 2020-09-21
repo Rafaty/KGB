fe0ef4a2c2ac81d507d0294c3d0df5c275c780bc
gui <guilhermesabinorocha@gmail.com>
2020-09-21 20:22:55 -0300

    cadastro e editar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Home from './pages/Home';
 import './App.css';
+import Register from './pages/Cadastro/index';
+import EditPage from './pages/Editar/index';
 
 function App() {
   return (
-    <Home/>
+    <EditPage/>
+    
   );
 }
 
